77f0d7a75fa1b6c16f4ae4a81add2c4fc00a60c0
vkr5959 <vkr5959@gmail.com>
2024-03-26 04:01:05 +0530

    - test name changed

--- tests/frontEnd/login/login.spec.js
+++ tests/frontEnd/login/login.spec.js
@@ -14,7 +14,7 @@
 
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('zoomcar page', async ({ page }) => {
   await page.goto('https://www.zoomcar.com/');
 // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Best Car Rental with Zoomcar | Explore & Drive Hassle-Free");
